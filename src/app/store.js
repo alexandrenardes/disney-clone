7a46668dbf4898e44bfefef67f24ac5d1a553c24
Alexandre Nardes <alexandrenardes00@gmail.com>
2024-07-08 23:08:57 -0300

    finalização de categorias e push de informações dos filmes como ex: banner, titulo, descrição do firebase para a aplicação.

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice'; // Certifique-se de que este caminho está correto
+import movieReducer from '../features/movie/movieSlice';
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        movie: movieReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false,
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,72 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Original from "./Original";
+import Trending from "./Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
+import { collection, onSnapshot } from "firebase/firestore"; // Importando as funções necessárias
 
 const Home = (props) => {
-    return (<Container>
-            <ImgSlider/>
-            <Viewers />
-            </Container>
-            )
-}
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  useEffect(() => {
+    console.log("hello");
+    const moviesCollection = collection(db, "movies"); // Referência à coleção "movies"
+    onSnapshot(moviesCollection, (snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        console.log(recommends);
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+
+          case "trending":
+            trending = [...trending, { id: doc.id, ...doc.data() }];
+            break;
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
+
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Original />
+      <Trending />
+    </Container>
+  );
+};
 
 const Container = styled.main`
   position: relative;
@@ -19,8 +77,7 @@ const Container = styled.main`
   padding: 0 calc(3.5vw + 5px);
 
   &:after {
-    background: url("/assets/home-background.png") center center / cover
-      no-repeat fixed;
+    background: url("/assets/home-background.png") center center / cover no-repeat fixed;
     content: "";
     position: absolute;
     inset: 0px;
